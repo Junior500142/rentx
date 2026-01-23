c4e20a1068acf2f7f5215e75b6412d5c4a3f96a1
Junior500142 <eliassjuniior1423@gmail.com>
2026-01-23 14:42:18 -0400

    CreateRentalUseCase with business rules

---
+++ src/aplication/useCases/createRental/createRentalUseCase.ts
@@ -0,0 +1,37 @@
+import { inject, injectable } from "inversify";
+import dayjs from "dayjs";
+import { TYPES } from "../../../infra/container/types";
+import { IRentalRepository } from "../../../domain/repositories/IRentalRepository";
+import { ICarRepository } from "../../../domain/repositories/ICarRepository";
+import { Rental } from "../../../domain/entities/Rental";
+import { ICreateRentalDTO } from "./CreateRentalDTO";
+
+@injectable()
+export class CreateRentalUseCase {
+  constructor(
+    @inject(TYPES.IRentalRepository) private rentalRepository: IRentalRepository,
+    @inject(TYPES.ICarRepository) private carRepository: ICarRepository
+  ) {}
+
+  async execute({ user_id, car_id, expected_return_date }: ICreateRentalDTO): Promise<Rental> {
+    const minimumHour = 24;
+
+    const carUnavailable = await this.rentalRepository.findOpenRentalByCar(car_id);
+    if (carUnavailable) throw new Error("Car is unavailable");
+
+    const rentalOpenToUser = await this.rentalRepository.findOpenRentalByUser(user_id);
+    if (rentalOpenToUser) throw new Error("There's a rental in progress for user!");
+
+    const dateNow = new Date();
+    const compareTime = dayjs(expected_return_date).diff(dateNow, "hours");
+
+    if (compareTime < minimumHour) throw new Error("Invalid return time!");
+
+    const rental = new Rental(Math.random().toString(), car_id, user_id, expected_return_date);
+    
+    await this.rentalRepository.create(rental);
+    await this.carRepository.updateAvailability(car_id, false);
+
+    return rental;
+  }
+}
