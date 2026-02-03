0712f82a7b3177e2f4a9b588e2148aed55db2ebc
Junior500142 <eliassjuniior1423@gmail.com>
2026-02-02 20:18:54 -0400

    fix(infra): use shared Prisma client in PrismaCarRepository

--- src/infra/database/prisma/prismaCarRepository.ts
+++ src/infra/database/prisma/prismaCarRepository.ts
@@ -1,21 +1,22 @@
 import { injectable } from "inversify";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "./client";
 import { ICarRepository } from "../../../domain/repositories/ICarRepository";
-
-const prisma = new PrismaClient();
 
 @injectable()
 export class PrismaCarRepository implements ICarRepository {
-
   async findById(id: string): Promise<any> {
     return prisma.car.findUnique({
       where: { id },
     });
   }
 
-  async findByLicensePlate(license_plate: string): Promise<any> {
-    return prisma.car.findFirst({
-      where: { license_plate },
+  async updateAvailability(
+    car_id: string,
+    available: boolean
+  ): Promise<void> {
+    await prisma.car.update({
+      where: { id: car_id },
+      data: { available },
     });
   }
 }
